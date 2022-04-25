5ab6fcca471bda5cdf281fd98bdd3267edae5350
kreako <olivier@kreako.fr>
2022-04-25 14:11:25 +0200

    Add getItemList and add keys management for react-query cache mechanism

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,9 +1,23 @@
 import axios from "axios"
-import { components, operations } from "./backend"
+import { QueryFunctionContext } from "react-query"
+import { components } from "./backend"
 
 type Item = components["schemas"]["Item"]
+type ItemList = components["schemas"]["ItemList"]
 
-export const getItem = async (): Promise<Item> => {
+export const keysItem = {
+  list: [{ scope: "list" }] as const,
+  item: (id: number) => [{ scope: "item", id }] as const,
+}
+
+export const getItemList = async (): Promise<ItemList> => {
   const response = await axios.get("http://127.0.0.1:3000/api/", { timeout: 1000 })
+  return response.data
+}
+
+export const getItem = async ({
+  queryKey: [{ id }],
+}: QueryFunctionContext<ReturnType<typeof keysItem["item"]>>): Promise<Item> => {
+  const response = await axios.get(`http://127.0.0.1:3000/api/${id}`, { timeout: 1000 })
   return response.data
 }
